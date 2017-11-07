80760fcbea756b73558ec811d65402c661b8dcaa
David <davjue@gmail.com>
2017-11-06 20:58:13 -0800

    trying to get rid of cors issue

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,11 @@ module Yugioh
     config.generators.system_tests = nil
     config.assets.compile = true
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
