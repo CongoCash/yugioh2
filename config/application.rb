6ee2a99f286d16ab7437f94d61656feeea813c35
David <davjue@gmail.com>
2017-11-06 21:17:51 -0800

    removed cors stuff

--- config/application.rb
+++ config/application.rb
@@ -30,11 +30,6 @@ module Yugioh
     config.generators.system_tests = nil
     config.assets.compile = true
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
-
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
