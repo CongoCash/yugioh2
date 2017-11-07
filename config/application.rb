edac7ea031a9b714fe75023a46072ea0bb8787f9
David <davjue@gmail.com>
2017-11-07 00:46:06 -0800

    changed get to any

--- config/application.rb
+++ config/application.rb
@@ -30,20 +30,15 @@ module Yugioh
     config.generators.system_tests = nil
     config.assets.compile = true
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
-
-    config.action_dispatch.default_headers.merge!({
-                                                    'Access-Control-Allow-Origin' => '*',
-                                                    'Access-Control-Request-Method' => '*'
-                                                  })
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Origin' => '*',
+    #   'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    # }
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => :any
       end
     end
   end
